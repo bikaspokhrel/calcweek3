24aaa0cb822ba682f17d4e38c5ab83ac3a98826d
Bikash Pokhrel <bpokhrel@aut.ac.nz>
2019-01-24 11:24:49 +1300

    add new button

--- calcweek3/Form1.Designer.cs
+++ calcweek3/Form1.Designer.cs
@@ -36,13 +36,15 @@
             this.txtNum2 = new System.Windows.Forms.TextBox();
             this.lbxAnswer = new System.Windows.Forms.ListBox();
             this.lblAnswer = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnMultiply
             // 
-            this.btnMultiply.Location = new System.Drawing.Point(252, 122);
+            this.btnMultiply.Location = new System.Drawing.Point(336, 150);
+            this.btnMultiply.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnMultiply.Name = "btnMultiply";
-            this.btnMultiply.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiply.Size = new System.Drawing.Size(100, 28);
             this.btnMultiply.TabIndex = 0;
             this.btnMultiply.Text = "*";
             this.btnMultiply.UseVisualStyleBackColor = true;
@@ -50,9 +52,10 @@
             // 
             // btnDivide
             // 
-            this.btnDivide.Location = new System.Drawing.Point(252, 151);
+            this.btnDivide.Location = new System.Drawing.Point(336, 186);
+            this.btnDivide.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnDivide.Name = "btnDivide";
-            this.btnDivide.Size = new System.Drawing.Size(75, 23);
+            this.btnDivide.Size = new System.Drawing.Size(100, 28);
             this.btnDivide.TabIndex = 1;
             this.btnDivide.Text = "/";
             this.btnDivide.UseVisualStyleBackColor = true;
@@ -60,9 +63,10 @@
             // 
             // btnMinus
             // 
-            this.btnMinus.Location = new System.Drawing.Point(252, 180);
+            this.btnMinus.Location = new System.Drawing.Point(336, 222);
+            this.btnMinus.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnMinus.Name = "btnMinus";
-            this.btnMinus.Size = new System.Drawing.Size(75, 23);
+            this.btnMinus.Size = new System.Drawing.Size(100, 28);
             this.btnMinus.TabIndex = 2;
             this.btnMinus.Text = "-";
             this.btnMinus.UseVisualStyleBackColor = true;
@@ -70,9 +74,10 @@
             // 
             // btnPlus
             // 
-            this.btnPlus.Location = new System.Drawing.Point(252, 209);
+            this.btnPlus.Location = new System.Drawing.Point(336, 257);
+            this.btnPlus.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnPlus.Name = "btnPlus";
-            this.btnPlus.Size = new System.Drawing.Size(75, 23);
+            this.btnPlus.Size = new System.Drawing.Size(100, 28);
             this.btnPlus.TabIndex = 3;
             this.btnPlus.Text = "+";
             this.btnPlus.UseVisualStyleBackColor = true;
@@ -80,41 +85,56 @@
             // 
             // txtNum1
             // 
-            this.txtNum1.Location = new System.Drawing.Point(57, 66);
+            this.txtNum1.Location = new System.Drawing.Point(76, 81);
+            this.txtNum1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNum1.Name = "txtNum1";
-            this.txtNum1.Size = new System.Drawing.Size(100, 20);
+            this.txtNum1.Size = new System.Drawing.Size(132, 22);
             this.txtNum1.TabIndex = 4;
             // 
             // txtNum2
             // 
-            this.txtNum2.Location = new System.Drawing.Point(217, 66);
+            this.txtNum2.Location = new System.Drawing.Point(289, 81);
+            this.txtNum2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNum2.Name = "txtNum2";
-            this.txtNum2.Size = new System.Drawing.Size(100, 20);
+            this.txtNum2.Size = new System.Drawing.Size(132, 22);
             this.txtNum2.TabIndex = 5;
             // 
             // lbxAnswer
             // 
             this.lbxAnswer.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.lbxAnswer.FormattingEnabled = true;
-            this.lbxAnswer.Location = new System.Drawing.Point(57, 122);
+            this.lbxAnswer.ItemHeight = 16;
+            this.lbxAnswer.Location = new System.Drawing.Point(76, 150);
+            this.lbxAnswer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lbxAnswer.Name = "lbxAnswer";
-            this.lbxAnswer.Size = new System.Drawing.Size(120, 95);
+            this.lbxAnswer.Size = new System.Drawing.Size(159, 116);
             this.lbxAnswer.TabIndex = 6;
             // 
             // lblAnswer
             // 
             this.lblAnswer.BackColor = System.Drawing.SystemColors.Info;
-            this.lblAnswer.Location = new System.Drawing.Point(57, 93);
+            this.lblAnswer.Location = new System.Drawing.Point(76, 114);
+            this.lblAnswer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblAnswer.Name = "lblAnswer";
-            this.lblAnswer.Size = new System.Drawing.Size(100, 23);
+            this.lblAnswer.Size = new System.Drawing.Size(133, 28);
             this.lblAnswer.TabIndex = 7;
             this.lblAnswer.Text = "label1";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(277, 375);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(367, 450);
+            this.ClientSize = new System.Drawing.Size(489, 554);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lblAnswer);
             this.Controls.Add(this.lbxAnswer);
             this.Controls.Add(this.txtNum2);
@@ -123,6 +143,7 @@
             this.Controls.Add(this.btnMinus);
             this.Controls.Add(this.btnDivide);
             this.Controls.Add(this.btnMultiply);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -141,6 +162,7 @@
         private System.Windows.Forms.TextBox txtNum2;
         private System.Windows.Forms.ListBox lbxAnswer;
         private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Button button1;
     }
 }
 
